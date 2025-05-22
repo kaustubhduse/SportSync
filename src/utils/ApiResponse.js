aa6231895157cc66d154bc1953f1ec1a1620a8ab
kaustubhduse <202251045@iiitvadodara.ac.in>
2025-05-22 19:00:51 +0000

    Remove all files from remote

--- src/utils/ApiResponse.js
+++
@@ -1,9 +0,0 @@
-class ApiResponse{
-    constructor(statusCode,  data,message ="scuccess"){
-        this.success = statusCode<400;
-        this.message = message;
-        this.data = data;
-        this.statusCode = statusCode;
-    }
-}
-export default ApiResponse;
